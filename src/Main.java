1d7b20b4dbb71b13360fb7447bdb78db484202b6
Emmanuel Lopez Cabrera <emmanuellopezcabrera333@gmail.com>
2026-09-21 15:04:35 -0500

    esta vez avanze el proyecto a lo que pide el profesor de la clase

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        TamaGotchi miMascota = new TamaGotchi();
+        miMascota.asignarNombre("Peresonsin");
+        System.out.println("¡Una nueva mascota ha nacido!");
+        miMascota.mostrarEstado();
+        miMascota.jugar();
+        miMascota.jugar();
+        miMascota.mostrarEstado();
+        miMascota.alimentar();
+        miMascota.mostrarEstado();
+    }
+}
--- src/TamaGotchi.java
+++ src/TamaGotchi.java
@@ -1,13 +1,30 @@
 public class TamaGotchi {
     private String nombre;
-    private int hambre;
-    private int diversion;
-    private int energia;
-    private int vida;
+    private int hambre = 100;
+    private int energia = 0;
 
-    public TamaGotchi(String nombre){
-        this.nombre = nombre;
-        this.hambre = 100;
-
+    public void asignarNombre(String nuevoNombre) {
+        this.nombre = nuevoNombre;
     }
+
+    public void jugar() {
+        this.energia -= 20;
+        this.hambre += 10;
+        System.out.println(this.nombre + " está jugando y saltando!");
+    }
+
+    public void alimentar() {
+        this.energia += 15;
+        this.hambre -= 20;
+        System.out.println(this.nombre + " está comiendo su alimento favorito!");
+    }
+
+    public void mostrarEstado() {
+        System.out.println("=== ESTADO DE TU TAMAGOTCHI ===");
+        System.out.println("Nombre: " + this.nombre);
+        System.out.println("Energia: " + this.energia);
+        System.out.println("Hambre: " + this.hambre);
+        System.out.println("--------------------------------");
+    }
+
 }
